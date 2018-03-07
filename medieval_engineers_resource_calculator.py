90f7fed5c7907783e13ee7bb60b533db23bf420f
themadtinker <37154043+themadtinker@users.noreply.github.com>
2018-03-07 11:49:41 -0600

    medieval_engineers_resource_calculator.py
    tool to calculate raw materials needed for crafting items.

---
+++ medieval_engineers_resource_calculator.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+items = dict()
+
+
+def create_tables():
+    global items
+    items['sticks'] = {'log': (1/15)}
+    items['timber'] = {'log': (1/4)}
+    items['planks'] = {'timber': (1/4)}
+    items['metal parts'] = {'iron ingot': (1/10)}
+#     items['iron ingot'] = {'iron ore': 6}
+    items['large chest'] = {'planks': 8, 'metal parts': 4}
+    items['seeder'] = {'large chest': 1, 'timber': 10, 'metal parts': 18}
+    items['plow'] = {'sticks': 15, 'timber': 4, 'metal parts': 60}
+    items['harvester'] = {'large chest': 1, 'timber': 10, 'metal parts': 16}
+    items['farm unit'] = {'harvester': 1, 'plow': 1, 'seeder': 1}
+
+
+def get_components(item_name, item_count):
+    if len(item_name) > 0 and item_name in items.keys():
+        temp_item = dict(items[item_name])  # create a local copy of the item
+        for component in temp_item:
+            temp_item[component] *= item_count
+            new_components = get_components(component, temp_item[component])
+            if len(new_components) > 0:
+                temp_item[component] = 0
+            temp_item = dict(Counter(temp_item) + Counter(new_components))
+        return temp_item
+    else:
+        return dict()
+
+
+if __name__ == '__main__':
+    create_tables()
+    a = get_components('farm unit', 1)
+    print(a)
